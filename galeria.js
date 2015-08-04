f3b56c33f9de1e0eeb395ccc2a2b2f4c6e56b2c6
gabriela1996 <aglaguado8@misena.edu.co>
2015-08-04 17:39:39 -0500

    inicial

---
+++ galeria.js
@@ -0,0 +1,18 @@
+function hola2() {
+	document.getElementById('imgprincipal').src="./imagenes/images (2).jpg";
+	
+}
+function hola3() {
+	document.getElementById('imgprincipal').src="./imagenes/images (3).jpg";
+}
+function hola4() {
+	document.getElementById('imgprincipal').src="./imagenes/images (4).jpg";
+}
+function hola5() {
+	document.getElementById('imgprincipal').src="./imagenes/images (5).jpg";
+}
+
+function volver() {
+	document.getElementById('imgprincipal').src="./imagenes/imagenprincipal.jpg";
+}
+
